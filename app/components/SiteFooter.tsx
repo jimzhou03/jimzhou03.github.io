69af08d0e6ea5c1e01f3a3988980f719f03f3447
jimzhou03 <2500975207@qq.com>
2026-07-31 22:08:05 +0800

    Refine life gallery and footer

--- app/components/SiteFooter.tsx
+++ app/components/SiteFooter.tsx
@@ -1,11 +1,8 @@
 export default function SiteFooter() {
   return (
     <footer className="site-footer">
-      <div>
-        <b>WEIJIE ZHOU</b>
-        <small>模型 / RETRIEVAL / KNOWLEDGE</small>
-      </div>
-      <p>© 2026 · FROM LANGUAGE DATA TO RELIABLE SYSTEMS</p>
+      <b>WEIJIE ZHOU</b>
+      <a href="mailto:weijiezhou03@outlook.com">WEIJIEZHOU03@OUTLOOK.COM</a>
       <a href="https://github.com/jimzhou03" target="_blank" rel="noreferrer">
         GITHUB ↗
       </a>
